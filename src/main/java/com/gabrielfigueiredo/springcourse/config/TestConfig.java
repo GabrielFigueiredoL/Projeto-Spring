c5e27b87e1bd6b7d8bca7bf4e2d217b40dd4d900
gabrielfigueiredol <gabrieellfigueiredo@gmail.com>
2024-08-20 22:21:29 -0300

    feat: add fake categories to test db

--- src/main/java/com/gabrielfigueiredo/springcourse/config/TestConfig.java
+++ src/main/java/com/gabrielfigueiredo/springcourse/config/TestConfig.java
@@ -1,8 +1,10 @@
 package com.gabrielfigueiredo.springcourse.config;
 
+import com.gabrielfigueiredo.springcourse.entities.Category;
 import com.gabrielfigueiredo.springcourse.entities.Order;
 import com.gabrielfigueiredo.springcourse.entities.User;
 import com.gabrielfigueiredo.springcourse.entities.enums.OrderStatus;
+import com.gabrielfigueiredo.springcourse.repositories.CategoryRepository;
 import com.gabrielfigueiredo.springcourse.repositories.OrderRepository;
 import com.gabrielfigueiredo.springcourse.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,6 +24,8 @@ public class TestConfig implements CommandLineRunner {
     @Autowired
     private OrderRepository orderRepository;
 
+    @Autowired
+    private CategoryRepository categoryRepository;
 
     @Override
     public void run(String... args) throws Exception {
@@ -31,8 +35,14 @@ public class TestConfig implements CommandLineRunner {
         Order order1 = new Order(null, user1, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.CANCELED);
         Order order2 = new Order(null, user2, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.PAID);
         Order order3 = new Order(null, user1, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.SHIPPED);
+
+        Category category1 = new Category(null, "Electronics");
+        Category category2 = new Category(null, "Books");
+        Category category3 = new Category(null, "Computers");
+
         userRepository.saveAll(Arrays.asList(user1, user2));
         orderRepository.saveAll(Arrays.asList(order1, order2, order3));
+        categoryRepository.saveAll(Arrays.asList(category1, category2, category3));
     }
 
 
