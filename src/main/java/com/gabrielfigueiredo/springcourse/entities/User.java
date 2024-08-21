94ffc61ec7322f90ffb8e687597e422dd9eccfc4
gabrielfigueiredol <gabrieellfigueiredo@gmail.com>
2024-08-20 21:49:47 -0300

    feat: add orders db dependency to user class

--- src/main/java/com/gabrielfigueiredo/springcourse/entities/User.java
+++ src/main/java/com/gabrielfigueiredo/springcourse/entities/User.java
@@ -1,9 +1,12 @@
 package com.gabrielfigueiredo.springcourse.entities;
 
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 @Entity
@@ -18,6 +21,9 @@ public class User implements Serializable {
     private String email;
     private String phone;
     private String password;
+
+    @OneToMany(mappedBy = "client")
+    private List<Order> orders = new ArrayList<Order>();
 
     public User() {}
 
@@ -67,6 +73,10 @@ public class User implements Serializable {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public void setOrders(List<Order> orders) {
+        this.orders = orders;
     }
 
     @Override
