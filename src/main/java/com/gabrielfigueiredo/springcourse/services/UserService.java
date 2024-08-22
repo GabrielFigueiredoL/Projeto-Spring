96f6b58f2b2b192e4e393c9148aa969a4b5b8c4a
gabrielfigueiredol <gabrieellfigueiredo@gmail.com>
2024-08-22 20:33:28 -0300

    feat: implement not found exception handler to find by id

--- src/main/java/com/gabrielfigueiredo/springcourse/services/UserService.java
+++ src/main/java/com/gabrielfigueiredo/springcourse/services/UserService.java
@@ -2,6 +2,7 @@ package com.gabrielfigueiredo.springcourse.services;
 
 import com.gabrielfigueiredo.springcourse.entities.User;
 import com.gabrielfigueiredo.springcourse.repositories.UserRepository;
+import com.gabrielfigueiredo.springcourse.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -19,7 +20,7 @@ public class UserService {
 
     public User findById(Long id) {
        Optional<User> user = userRepository.findById(id);
-       return user.get();
+       return user.orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
     public User insert(User user) {
